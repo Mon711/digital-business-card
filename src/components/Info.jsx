ec74d504b929f029fb5bdf588a43eb482b021d23
Mon711 <mrinalsood711@gmail.com>
2025-11-06 01:24:35 +0530

    make styling changes

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -7,7 +7,7 @@ export default function Info(){
 
                 <div className="card-body">
                     <h2 className="card-name">Mrinal Sood</h2>
-                    <p2 className="card-title">Full-Stack Developer</p2>
+                    <p className="card-title">Full-Stack Developer</p>
                     <a href="https://mon711.github.io/aboutMe/" className="card-link">mon711.aboutme</a>
 
                     <div className="card-buttons">
