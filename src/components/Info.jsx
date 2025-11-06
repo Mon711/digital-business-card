ba2a8671029c159cdc40a85f96c581f2f1fded0a
Mon711 <mrinalsood711@gmail.com>
2025-11-07 01:20:31 +0530

    add interest and about components. Style them. Clean up some css

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,28 +1,44 @@
-export default function Info(){
-    return (
-        <>
-            <div className="card">
+import About from "./About";
+import Interest from "./Interest";
 
-                <img src="src/assets/my_pic.jpeg" alt="authors profile pic" className="card-img"/>
+export default function Info() {
+  return (
+    <>
+      <div className="card">
+        <img
+          src="src/assets/my_pic.jpeg"
+          alt="authors profile pic"
+          className="card-img"
+        />
 
-                <div className="card-body">
-                    <h2 className="card-name">Mrinal Sood</h2>
-                    <p className="card-title">Full-Stack Developer</p>
-                    <a href="https://mon711.github.io/aboutMe/" className="card-link">mon711.aboutme</a>
+        <div className="card-body">
+          <h2 className="card-name">Mrinal Sood</h2>
+          <p className="card-title">Budding Web Devleoper</p>
+          <a href="https://mon711.github.io/aboutMe/" className="card-link">
+            mon711.aboutme
+          </a>
 
-                    <div className="card-buttons">
-                        <button className="btn email">
-                            <i class="bi bi-envelope-at-fill"></i>
-                            <span className="btn-text">Email</span>
-                        </button>
-                        <button className="btn linkedin">
-                            <i className="bi bi-linkedin"></i>
-                            <span className="btn-text">LinkedIn</span>
-                        </button>
-                    </div>
-                </div>
-                
-            </div>
-        </>
-    )
+          <div className="card-buttons">
+            <button className="btn email">
+              <i class="bi bi-envelope-at-fill"></i>
+              <span className="btn-text">Email</span>
+            </button>
+            <button className="btn linkedin">
+              <i className="bi bi-linkedin"></i>
+              <span className="btn-text">LinkedIn</span>
+            </button>
+          </div>
+
+          <div className="card-section">
+            <About />
+          </div>
+
+          <div className="card-section">
+            <Interest />
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ src/components/Interest.jsx
@@ -0,0 +1,12 @@
+export default function Interest() {
+    return (
+        <>
+            <h3>Interests</h3>
+            <p>
+              Playing the guitar, Singing bollywood and punjabi songs, reading
+              non-fiction, dabbling in Stocks,gymming, reading up on AI/ML and
+              learning Math are some of my interests besides coding.
+            </p>
+        </>
+    )
+}
